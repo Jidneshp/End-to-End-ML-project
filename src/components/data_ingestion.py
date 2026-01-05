ce0433f57245fd6109e9ae3c0c0656971f0462c4
Jidneshp <jidneshpatil866@gmail.com>
2026-01-05 17:23:03 +0530

    'data_transformation'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,13 +2,16 @@ import os
 import sys
 # Add project root to Python path to allow imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
+
+from src.components.data_transformation import DatatransformationConfig
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -50,4 +53,8 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
